798718412ce45fe21f14d9abebb6b4a3cd056392
Andrew A <andrewadev@gmail.com>
2022-02-04 21:07:35 +0100

    Button click now changes page background
    Note that this will error out if run on internal Chrome pages (that an error is the result could've been made more clear in the Chrome docs)
    
    https://developer.chrome.com/docs/extensions/mv3/getstarted/#logic

--- popup.js
+++ popup.js
@@ -3,3 +3,22 @@ let changeColor = document.getElementById('changeColor');
 chrome.storage.sync.get('color', ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
+
+
+function setPageBackoundColor() {
+  chrome.storage.sync.get('color', ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
+
+
+// Inject func to set background color on click
+changeColor.addEventListener('click', async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true});
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackoundColor,
+  });
+});
+
